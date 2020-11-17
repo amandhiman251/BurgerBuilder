a34e2017f173b780685bb7ebed9067e886e4a926
Aman Dhiman <amandhiman251@gmail.com>
2020-11-17 22:10:27 +0530

    adding a SideDrawer for mobile sites

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import classes from './SideDrawer.module.css';
+import Logo from '../../../components/Logo/Logo';
+import NavigationItem from '../Navigationitems/NavigationItem/NavigationItem';
+
+const sideDrawer = () => {
+    return (
+        <div className={classes.SideDrawer}>
+            <Logo />
+            <nav>
+                <NavigationItem>BurgerBuilder</NavigationItem>
+                <NavigationItem>Checkout</NavigationItem>
+            </nav>
+        </div>
+    );
+}
+export default sideDrawer;
