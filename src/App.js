3d9f77153c46dab5fa73e5e21f26bb053bcb51e9
Aman Dhiman <amandhiman251@gmail.com>
2020-12-13 16:17:13 +0530

    another way is switch but order of routes is must here

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 
 class App extends Component {
@@ -11,8 +11,10 @@ class App extends Component {
         return (
             <div>
             <Layout>
-                <Route path='/' exact component={BurgerBuilder} />
-                <Route path='/checkout' component={Checkout} />
+                <Switch>
+                    <Route path='/checkout' component={Checkout} />
+                    <Route path='/' component={BurgerBuilder} />
+                </Switch>
             </Layout>
             </div>
         );
