941d48288d74ed08dcf922035b05a467b1143c3c
Aman Dhiman <amandhiman251@gmail.com>
2021-01-02 23:14:20 +0530

    The Order Reducer

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -2,15 +2,27 @@ import * as actionTypes from '../actions/actionTypes';
 
 
 const initialState = {
-    loading: false
+        orders:[],
+        loading:false
 }
 
 const reducer = (state=initialState, action) => {
     switch (action.type) {
         case actionTypes.PURCHASE_BURGER_SUCCESS:
+            const newOrder ={
+                ...action.orderData,
+                id: action.orderID
+            };
            return{
-                    loading:false
+               ...state,
+               loading:false,
+               orders: state.orders.concat(newOrder)
            }
+        case actionTypes.PURCHASE_BURGER_FAIL:
+            return{
+                ...state,
+                loading:false
+            }
         default: return state;
     }
 }
